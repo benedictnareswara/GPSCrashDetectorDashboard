0ff7164638e4a5b379899bdee0555c6dc966b8e4
benedictnareswara <benedictnareswara@users.noreply.github.com>
2026-04-28 11:59:13 +0000

    deploy: b5db1834b81739cca63003a44d797f862226499d

--- server/assets/_tanstack-start-manifest_v-Dmgraoi_.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/GPSCrashDetectorDashboard/GPSCrashDetectorDashboard/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DvULLigr.js"] }, "/": { filePath: "/home/runner/work/GPSCrashDetectorDashboard/GPSCrashDetectorDashboard/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BrorX8tX.js"] } }, clientEntry: "/assets/index-DvULLigr.js" });
-export {
-  tsrStartManifest
-};
